b0086692d280154efee4108575df6ae327ab970c
Abhishek1923 <61440093+Abhishek1923@users.noreply.github.com>
2022-03-18 20:12:20 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="Abhishek"&& password=="1923")
+{
+    alert("login succesfully");
+    return false;
+
+}
+else
+{
+    alert("login failed");
+}
+}
